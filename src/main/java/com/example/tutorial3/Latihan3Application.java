842e0bc87b192462b5c26359eded7b17c914ede6
Amira Luthfia Fitriani <amirafizh17@gmail.com>
2017-09-21 18:27:15 +0700

    tutorial 3

---
+++ src/main/java/com/example/tutorial3/Latihan3Application.java
@@ -0,0 +1,12 @@
+package com.example.tutorial3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Latihan3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Latihan3Application.class, args);
+	}
+}
---
+++ src/main/java/com/example/tutorial3/service/InMemoryStudentService.java
@@ -0,0 +1,39 @@
+package com.example.tutorial3.service;
+
+import com.example.tutorial3.model.StudentModel;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryStudentService implements StudentService {
+	private static List<StudentModel> studentList = new ArrayList<StudentModel>();
+	private String npm;
+	
+	@Override
+	public StudentModel selectStudent(String npm) {
+		int i = 0;
+		for (i = 0; i < studentList.size(); i++){
+			StudentModel student = studentList.get(i);
+			if(student.getNpm().equals(npm)){
+				return student;
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public List<StudentModel> selectAllStudents() {
+		return studentList;
+	}
+	
+	@Override
+	public void addStudent(StudentModel student){
+		studentList.add(student);
+	}
+	
+	
+
+
+
+
+
+}
